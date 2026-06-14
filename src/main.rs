8ff7bd525aea07d6ab4fc07f7d47ed87e202500d
Mokss <maksim.shuvaev@botmother.com>
2026-06-14 19:02:03 +0300

    14.06.2026 Graceful Shutdown

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,16 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+    for stream in listener.incoming().take(2) {
+    // for stream in listener.incoming() {
+        let stream: TcpStream = stream.unwrap();
 
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -44,7 +47,8 @@ fn handle_connection(mut stream: TcpStream) {
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
